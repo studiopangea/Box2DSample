7dbd67d12bf766940f4df362084d829b032ca700
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-10-09 14:45:11 -0300

    XCode4/iOS project ported to cocos2d-x 2.0.3.

---
+++ proj.ios/Box2DSample/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ProjectTemplateAppController.h
+//  ProjectTemplate
+//
+//  Created by Gonzalo Diaz Cruz on 30-09-12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ proj.ios/Box2DSample/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProjectTemplateAppController.h
+//  ProjectTemplate
+//
+//  Created by Gonzalo Diaz Cruz on 30-09-12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
